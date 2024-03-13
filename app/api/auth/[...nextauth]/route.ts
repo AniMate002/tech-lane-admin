850981981b39b3b1035b652cc6a23dfdc1744b55
Alukard <kirill1974111@gmail.com>
2024-03-13 21:56:09 +0100

    v0.14.7 Fixing Auth options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,40 +1,11 @@
-import { User } from "@/models/User"
-import { mongooseConnect } from "@/models/mongoose"
-import NextAuth, { AuthOptions, NextAuthOptions } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import bcrypt from "bcryptjs"
+
+import authOptions from "@/configs/authOptions"
+import NextAuth from "next-auth"
 
 
 
-export const authOptions:NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-        credentials: {
-            email: {label: 'Email', type: 'email', required: true},
-            password: {label: 'Password', type: 'password', required: true}
-        },
-        async authorize(credentials:any) {
-            if(!credentials?.email || !credentials?.password) return null
-            
-            try{
-                await mongooseConnect()
-                const foundUser = await User.findOne({ email: credentials.email })
-                if(foundUser){
-                    const isPasswordCorrect = await bcrypt.compare(credentials.password, foundUser.password)
-                    if(isPasswordCorrect){
-                        return foundUser
-                    }
-                }
-            }catch(e: any){
-                throw new Error(e)
-            }
-        },
-    })
-  ],
-  pages: {
-    signIn: '/login'
-  }
-}
+
+
 // export default NextAuth(authOptions)
 const handler = NextAuth(authOptions)
 
