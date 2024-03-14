7749006d5c1402739f6622841bf67831a0e68c97
AniMate002 <kirill1974111@gmail.com>
2024-03-14 17:03:29 +0100

    Removing Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import "./globals.css";
-import { getServerSession } from "next-auth";
-import SessionProvider from '../utils/SessionProvider'
 import { Nunito, Cabin, Inter } from "next/font/google";
 
 const nunito = Nunito({ subsets: ["latin"] });
@@ -14,17 +12,14 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await getServerSession()
   return (
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
-      <SessionProvider session={session}>
         <body className={inter.className}>
           {children}
         </body>
-      </SessionProvider>
     </html>
   );
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,19 +1,18 @@
 'use client'
-import { useSession } from "next-auth/react";
 import HeaderAccount from "./HeaderAccount";
 import HeaderButtons from "./HeaderButtons";
 import HeaderInput from "./HeaderInput";
 
 
 const Header = () => {
-    const session = useSession()
     return (
         <header className=" bg-blue-600 h-[300px] pt-5 px-10 flex justify-between items-start">
             <HeaderInput />
-            { session.status === 'authenticated' 
+            {/* { session.status === 'authenticated' 
             ? <HeaderAccount user={session.data.user}/>
             : <HeaderButtons />
-            }
+            } */}
+            <HeaderButtons />
         </header>
     );
 }
