4b5273b65b509f2535913af3f8f678114e4a181a
AniMate002 <kirill1974111@gmail.com>
2024-03-14 21:48:16 +0100

    Changed hierarchy

---
+++ app/categories/layout.tsx
@@ -0,0 +1,42 @@
+import Navigation from "@/components/Navigation/Navigation";
+import Header from "@/components/Header/Header";
+import OuterContainer from "@/components/Containers/OuterContainer";
+import { Nunito, Cabin, Inter } from "next/font/google";
+
+const nunito = Nunito({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
+const cabin = Cabin({ subsets: ["latin"] });
+
+
+
+export default function CategoriesLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex w-[100%] bg-white">
+        <Navigation />
+        <div className="flex-grow">
+            <Header />
+            <main>
+              <OuterContainer>
+                {children}
+              </OuterContainer>
+            </main>
+        </div>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -21,17 +21,7 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body className={inter.className}>
-        <div className="flex w-[100%] bg-white">
-          <Navigation />
-          <div className="flex-grow">
-            <Header />
-            <main>
-              <OuterContainer>
-                {children}
-              </OuterContainer>
-            </main>
-          </div>
-        </div>
+        {children}
       </body>
     </html>
   );
