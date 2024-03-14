de60679c7d4ae3b0940ecb537d5216e3a75daf12
AniMate002 <kirill1974111@gmail.com>
2024-03-14 18:59:56 +0100

    Cleaning

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,3 @@
-import "./globals.css";
-import { getServerSession } from "next-auth";
-import SessionProvider from '../utils/SessionProvider'
 import { Nunito, Cabin, Inter } from "next/font/google";
 
 const nunito = Nunito({ subsets: ["latin"] });
@@ -9,22 +6,24 @@ const cabin = Cabin({ subsets: ["latin"] });
 
 
 
-export default async function RootLayout({
+
+
+
+export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  const session = await getServerSession()
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
-      <SessionProvider session={session}>
+      {/* <SessionProvider session={session}> */}
         <body className={inter.className}>
           {children}
         </body>
-      </SessionProvider>
+      {/* </SessionProvider> */}
     </html>
-  );
+  )
 }
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+
+const MainPage = () => {
+    return (
+        <div>
+            Hello world
+        </div>
+    );
+}
+ 
+export default MainPage;
